291d3d2a44c2ff7fc749e2ed01aa545db302df2f
Jack Shi <jackshi1999@gmail.com>
2018-12-01 04:24:22 -0500

    first compilable deep learning model yayyyyygit add model.py git add model.py

--- model.py
+++ model.py
@@ -13,25 +13,36 @@ df = dc.clean_item_data()
 df = dc.cleanup_categoryid(df)
 
 # vectorize training input data
-_X_train, _, _X_test, Y_train, _, Y_test = dc.data_split(df, 0.8, 0, 0.2)
+_X_train, _, _X_test, Y_train, _, Y_test = dc.data_split(df, 0.7, 0.1, 0.2)
 vectorizer = CountVectorizer()
-X_train = vectorizer.fit_transform(_X_train)
-X_test = vectorizer.transform(_X_test)
+print(_X_train)
+
+x_train_list = []
+x_test_list = []
+for _, row in _X_train.iterrows():
+    x_train_list.append(row[0])
+
+for _, row in _X_test.iterrows():
+    x_test_list.append(row[0])
+
+vectorizer.fit(x_train_list)
+X_train = vectorizer.transform(x_train_list)
+X_test = vectorizer.transform(x_test_list)
 
 # Neural Network
 input_dim = X_train.shape[1] # Number of features
 model = Sequential()
 model.add(layers.Dense(100, input_dim=input_dim, activation='relu'))
-model.add(layers.Dense(63, activation='softmax'))
+model.add(layers.Dense(1, activation='softmax'))
 model.compile(loss='mean_absolute_error',
               optimizer='adam',
               metrics=['accuracy'])
 print(model.summary())
 history = model.fit(X_train, Y_train,
-                    epochs=1,
+                    epochs=10,
                     verbose=False,
                     validation_data=(X_test, Y_test),
-                    batch_size=11000)
+                    batch_size=5000)
 
 loss, accuracy = model.evaluate(X_train, Y_train, verbose=False)
 print("Training Accuracy: {:.4f}".format(accuracy))
