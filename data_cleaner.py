dbc618e982ae8844d7efab3b84af829790d98ffc
1jinwoo <justinwon97@gmail.com>
2018-12-01 03:21:46 -0500

    debug data_split

--- data_cleaner.py
+++ data_cleaner.py
@@ -5,6 +5,7 @@ data_cleaner.py
 
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 def read_search_strings(file_path='search_strings.csv'):
     '''
@@ -33,6 +34,43 @@ def cleanup_categoryid(df):
         else:
             df.at[j, 'categoryId'] = i
     return category_dict
+
+def data_split(df, train=0.65, valid=0.15, test=0.20):
+    """
+    split data into training, validation, and test sets
+    :param df: the data set
+    :param train: percentage of training data
+    :param valid: percentage of validation data
+    :param test: percentage of test data
+    :return: X_train, X_valid, X_test, Y_train, Y_valid, Y_test
+    """
+
+    # instantiate variables
+    column_headers = list(df.columns.values)
+    X_train = pd.DataFrame()
+    X_valid = pd.DataFrame()
+    X_test = pd.DataFrame()
+    Y_train = pd.DataFrame()
+    Y_valid = pd.DataFrame()
+    Y_test = pd.DataFrame()
+    
+    id_num = df['categoryId'].nunique()
+    for i in range(1, id_num+1):
+        x_category_df = df.loc[df['categoryId'] == i]['item_title']
+        y_category_df = df.loc[df['categoryId'] == i]['categoryId']
+
+        x_category_train_valid, x_category_test, y_category_train_valid, y_category_test = \
+            train_test_split(x_category_df, y_category_df, test_size=test)
+        x_category_train, x_category_valid, y_category_train, y_category_valid = \
+            train_test_split(x_category_train_valid, y_category_train_valid, train_size=train/(train+valid))
+        X_train = pd.concat([X_train, x_category_train], axis=0)
+        X_valid = pd.concat([X_valid, x_category_valid], axis=0)
+        X_test = pd.concat([X_test, x_category_test], axis=0)
+        Y_train = pd.concat([Y_train, y_category_train], axis=0)
+        Y_valid = pd.concat([Y_valid, y_category_valid], axis=0)
+        Y_test = pd.concat([Y_test, y_category_test], axis=0)
+
+    return X_train, X_valid, X_test, Y_train, Y_valid, Y_test
 
 
 if __name__ == '__main__':
