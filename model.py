686f202a3bdd7182e85a03a6b44a739da4f4b5fb
Jack Shi <jackshi1999@gmail.com>
2018-12-01 03:23:13 -0500

    create simple one-layer NN

--- model.py
+++ model.py
@@ -3,13 +3,15 @@ from keras.models import Sequential
 from keras import layers
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
 from pandas import DataFrame
+
 
 # file import
 import item_title_cleaner as itc
 
 df = itc.clean_item_data()
-
+id_num = df['categoryId'].nunique()
 
 def data_split(df, train=0.65, valid=0.15, test=0.20):
     """
@@ -48,7 +50,36 @@ def data_split(df, train=0.65, valid=0.15, test=0.20):
     return X_train, X_valid, X_test, Y_train, Y_valid, Y_test
 
 
-print(data_split(df))
+# vectorize training input data
+_X_train, _, _X_test, Y_train, _, Y_test = data_split(df, 0.8, 0, 0.2)
+vectorizer = CountVectorizer()
+X_train = vectorizer.fit_transform(_X_train)
+X_test = vectorizer.transform(_X_test)
+
+#
+input_dim = X_train.shape[1] # Number of features
+model = Sequential()
+model.add(layers.Dense(100, input_dim=input_dim, activation='relu'))
+model.add(layers.Dense(63, activation='softmax'))
+model.compile(loss='mean_absolute_error',
+              optimizer='adam',
+              metrics=['accuracy'])
+print(model.summary())
+history = model.fit(X_train, Y_train,
+                    epochs=1,
+                    verbose=False,
+                    validation_data=(X_test, Y_test),
+                    batch_size=11000)
+
+loss, accuracy = model.evaluate(X_train, Y_train, verbose=False)
+print("Training Accuracy: {:.4f}".format(accuracy))
+loss, accuracy = model.evaluate(X_test, Y_test, verbose=False)
+print("Testing Accuracy:  {:.4f}".format(accuracy))
+
+
+
+if __name__ == "__main__":
+    print(data_split(df))
 
 
 
