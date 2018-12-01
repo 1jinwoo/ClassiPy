1f83eaf80d536007dd18522297f6558e7d162134
1jinwoo <justinwon97@gmail.com>
2018-12-01 00:12:59 -0500

    add cleanup_categoryid()

--- data_cleaner.py
+++ data_cleaner.py
@@ -1,15 +1,38 @@
 
-# coding: utf-8
-
-# In[11]:
-
+'''
+data_cleaner.py
+'''
 
 import numpy as np
 import pandas as pd
 
-def read_search_strings():
-    df = pd.read_csv('search_strings.csv', header=0, sep=',', encoding='latin1')
+def read_search_strings(file_path='search_strings.csv'):
+    '''
+    Reads from csv from file_path
+    :return: pandas DataFrame of the csv
+    '''
+    df = pd.read_csv(file_path, header=0, sep=',', encoding='latin1')
     return df
 
 read_search_strings()
 
+def cleanup_categoryid():
+    '''
+    Assigns new category id starting from 1.
+    ** This function modifies df **
+    :return: dictionary[key] = categroyId
+    '''
+    i = 0
+    category_dict = dict()
+    for j, row in df.iterrows():
+        category = row[3]
+        if not category in category_dict.keys():
+            i += 1
+            category_dict[category] = i
+            df.at[j, 'categoryId'] = i
+        else:
+            df.at[j, 'categoryId'] = i
+    return category_dict
+
+print(cleanup_categoryid())
+df[100:200]
