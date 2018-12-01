25184cf9692f0234d55e154bcd52dff29344f46b
Jack Shi <jackshi1999@gmail.com>
2018-12-01 01:15:51 -0500

    create model.py and write data_split function, update README.md

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 ## Features
 Takes search string as input and output category for the product that the search string is aimed to find.
 
+## Dependence
+- NumPy
+- Pandas
+- TensorFlow
+- Keras
+- sklearn
+
 ## Contributors
 - Justin Won (@1jinwoo)
 - Jack Shi (@junyaoshi)
---
+++ model.py
@@ -0,0 +1,44 @@
+from keras.models import Sequential
+from keras import layers
+import numpy as np
+from sklearn.model_selection import train_test_split
+from pandas import DataFrame
+
+
+def data_split(df, train=0.65, valid=0.15, test=0.20):
+    """
+    split data into training, validation, and test sets
+    :param df: the data set
+    :param train: percentage of training data
+    :param valid: percentage of validation data
+    :param test: percentage of test data
+    :return: X_train, X_valid, X_test, Y_train, Y_valid, Y_test
+    """
+
+    # instantiate variables
+    column_headers = list(df.columns.values)
+    X_train = DataFrame(columns=column_headers)
+    X_valid = DataFrame(columns=column_headers)
+    X_test = DataFrame(columns=column_headers)
+    Y_train = DataFrame(columns=column_headers)
+    Y_valid = DataFrame(columns=column_headers)
+    Y_test = DataFrame(columns=column_headers)
+
+    id_num = df['categoryId'].nunique()
+    for i in range(1, id_num+1):
+        x_category_df = df.loc[df['categoryId'] == i]['item_title']
+        y_category_df = df.loc[df['categoryId'] == i]['categoryId']
+        x_category_train_valid, y_category_train_valid, x_category_test, y_category_test = \
+            train_test_split(x_category_df,y_category_df, test_size=test)
+        x_category_train, x_category_valid, y_category_train, y_category_valid = \
+            train_test_split(x_category_train_valid, y_category_train_valid, train_size=train/(train+valid))
+        X_train.append(x_category_train)
+        X_valid.append(x_category_valid)
+        X_test.append(x_category_test)
+        Y_train.append(y_category_train)
+        Y_train.append(x_category_valid)
+        Y_train.append(x_category_test)
+
+    return X_train, X_valid, X_test, Y_train, Y_valid, Y_test
+
+
